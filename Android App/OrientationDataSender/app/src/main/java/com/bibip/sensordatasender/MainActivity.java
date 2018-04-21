ffd16d2b18e46076a532d8a89990bc6d9707ba8c
szymon <szcz761@gmail.com>
2018-04-21 21:29:48 +0200

    Wysyłanie dzwieku po RTSP pierwsze podejście

--- Android App/OrientationDataSender/app/src/main/java/com/bibip/sensordatasender/MainActivity.java
+++ Android App/OrientationDataSender/app/src/main/java/com/bibip/sensordatasender/MainActivity.java
@@ -5,16 +5,24 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.net.InetAddress;
 
 public class MainActivity extends Activity implements OnClickListener {
 
     Button changeActivityButton;
     Intent sensorActivity;
-
+    CheckBox checkBoxAudio;
+    EditText editTextAudioIP;
+    EditText editTextAudioPORT;
+    GenericAudioStream audioStream;
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -26,7 +34,11 @@ public class MainActivity extends Activity implements OnClickListener {
         setContentView(R.layout.activity_main);
 
         sensorActivity = new Intent(this, SensorsActivity.class);
+
         changeActivityButton = (Button) findViewById(R.id.buttonStart);
+        checkBoxAudio = (CheckBox) findViewById(R.id.checkBoxSendAudio);
+        editTextAudioIP = (EditText) findViewById(R.id.EditTextAudioIP);
+        editTextAudioPORT = (EditText) findViewById(R.id.EditTextAudioPORT);
 
         //startActivity(sensorActivity);
 
@@ -49,6 +61,25 @@ public class MainActivity extends Activity implements OnClickListener {
             case R.id.buttonStart:
                 startActivity(sensorActivity);
                 break;
+            case R.id.checkBoxSendAudio:
+                sendingAudioCheck();
+                break;
+        }
+    }
+
+    private void sendingAudioCheck() {
+        try {
+            audioStream = new GenericAudioStream();
+            audioStream.setDestination(InetAddress.getByName(editTextAudioIP.toString()), Integer.parseInt(editTextAudioPORT.toString()));
+            if (checkBoxAudio.isChecked()) {
+                audioStream.start();
+            }
+            else {
+                audioStream.stop();
+            }
+        }
+        catch (Exception e){
+            Log.d("AudioSending",e.getMessage());
         }
     }
 }
